7e76dfbe7a29f068f1425461274df83160739e72
Alexander Berntsen <alexander@plaimi.net>
2014-11-14 15:27:28 +0100

    Initial commit
    Signed-off-by: Alexander Berntsen <alexander@plaimi.net>

---
+++ src/Authochan/Database.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+
+{- |
+Module      :  $Header$
+Description :  これはオートのデトベスです。
+Copyright   :  (c) plaimi 2014
+License     :  AGPL-3
+
+Maintainer  :  tempuhs@plaimi.net
+-} module Authochan.Database where
+
+import Data.ByteString
+  (
+  ByteString,
+  )
+import Data.Int
+  (
+  Int64,
+  )
+import Database.Persist.TH
+  (
+  mkMigrate,
+  mkPersist,
+  persistLowerCase,
+  share,
+  sqlSettings,
+  )
+import Data.Text
+  (
+  Text,
+  )
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+User
+  name           Text       -- user's ID on server, as text.
+  deriving Show
+Client
+  handle         ByteString -- sent by the client to tempauths.
+  secret         ByteString -- secret part of handle.
+  nonce          Int64      -- last message nonce.
+  user           UserId
+  description    Text
+  UniqueClient   handle
+  deriving Show
+|]
